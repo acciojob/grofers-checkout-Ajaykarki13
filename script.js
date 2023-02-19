1dc59dadd3a2b59710153614c896a733615509f4
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-02-19 18:20:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,13 +10,15 @@ for(let i=0;i<prices.length;i++)
 		{
 			sum = sum + parseInt( prices[i].innerHTML)
 		}
-	//let lastrow =  document.createElement("tr")
-	let lastrow =document.querySelector(".total")
-	lastrow.innerHTML =
-	`
-  <td>${sum}</td>` ;
+	let lastrow =  document.createElement("tr")
+	let table =document.querySelector("table")
+	let td =  document.createElement("td")
+	td.innerHTML = sum
+	//`
+ // <td>${sum}</td>` ;
 	//let table = document.querySelector(".table")
  // table.append(lastrow)
-
+lastrow.append(td)
+	table.append(lastrow)
 };
 getSumBtn.addEventListener("click", getSum);
