2ff5c9c273d95e6748b8da7192fb92bcda9c5ae3
Ajay Karki <115032238+Ajaykarki13@users.noreply.github.com>
2023-02-19 18:24:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+let prices=document.querySelectorAll(".price");
+let totalPrice=0;
+  for(let i=0;i<prices.length;i++){
+    totalPrice+=Number(prices[i].innerHTML);
+  }
+  console.log(totalPrice);
+  let row=document.createElement("tr");
+//   let td1=document.createElement("td");
+//   td1.textContent="Total Price";
+  let td2=document.createElement("td");
+  td2.textContent=totalPrice;
+  td2.setAttribute("id","ans");
+  row.append(td2);
+  document.querySelector("table").append(row);
+};
+
+getSumBtn.addEventListener("click", getSum);
+	
+	/*
   let prices = document.querySelectorAll(".price")
   let sum =0;
 for(let i=0;i<prices.length;i++)
@@ -22,3 +41,4 @@ lastrow.append(td)
 	table.append(lastrow)
 };
 getSumBtn.addEventListener("click", getSum);
+*/
